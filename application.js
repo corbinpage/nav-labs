dcbcac24baf10ee633cf7407a75fd1e58ca0384d
Corbin Page <corbin.page@gmail.com>
2015-07-01 18:59:47 -0400

    Finished version one

--- application.js
+++ application.js
@@ -2,23 +2,4 @@
 
   new WOW().init();
 
-  $("#modal-email-button").click(function() {
-    var url = "https://api.github.com/repos/corbinpage/nav-labs/issues";
-    data = {
-      "title": "Contact via Website Form",
-      "assignee": "corbinpage",
-      "body": "From: "+$("#modal-email-address").val()+" - "+$("#modal-email-body").val()
-    }
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: data,
-      success: function(data) {
-        console.log("data");
-      }
-    });
-
-  })  
-
-
 })();
