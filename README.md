8ec74b5598a6b87d2fcc6ea4fd1ecb74bf57d3c6
Corbin Page <corbin.page@gmail.com>
2016-04-01 07:51:32 -0400

    New logo and SVG inprogress

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Static website For NavLabs.
 https://www.draw.io/
 http://jeremyckahn.github.io/stylie/
 https://24ways.org/2013/animating-vectors-with-svg/
+http://maxwellito.github.io/vivus/
--- assets/js/app.js
+++ assets/js/app.js
@@ -12,62 +12,85 @@
     
     // line.plot(100, 250, 500, 250);
 
-    var start = SVG.select('#start')
+    // var start = SVG.select('#start')
 
-    start.attr({
-      fill: '#f06'
-      , 'fill-opacity': 0.5
-      , stroke: '#000'
-    })
+    // start.attr({
+    //   fill: '#f06'
+    //   , 'fill-opacity': 0.5
+    //   , stroke: '#000'
+    // })
 
-  } else {
-    console.log('SVG not supported')
-  }
-
-var current_frame, total_frames, path, length, handle, myobj;
-
-myobj = document.getElementById('drawing').cloneNode(true);
-
-var init = function() {
-  current_frame = 0;
-  total_frames = 60;
-  path = new Array();
-  length = new Array();
-  for(var i=0; i<8;i++){
-    path[i] = document.getElementById('i'+i);
-    console.log(i);
-    l = path[i].getTotalLength();
-    length[i] = l;
-    path[i].style.strokeDasharray = l + ' ' + l; 
-    path[i].style.strokeDashoffset = l;
-  }
-  handle = 0;
+} else {
+  console.log('SVG not supported')
 }
- 
- 
-var draw = function() {
-   var progress = current_frame/total_frames;
-   if (progress > 1) {
-     window.cancelAnimationFrame(handle);
-   } else {
-     current_frame++;
-     for(var j=0; j<path.length;j++){
-         path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
-     }
-     handle = window.requestAnimationFrame(draw);
-   }
-};
 
-init();
-draw();
+// var current_frame, total_frames, path, length, handle, myobj;
 
-var rerun = function() {
-  var old = document.getElementsByTagName('div')[0];
-  old.parentNode.removeChild(old);
-  document.getElementsByTagName('body')[0].appendChild(myobj);
-  init();
-  draw();
-};
+// myobj = document.getElementById('drawing').cloneNode(true);
+
+// var init = function() {
+//   current_frame = 0;
+//   total_frames = 60;
+//   path = new Array();
+//   length = new Array();
+//   var path2 = $(".moving-path");
+
+//   console.log(path2);
+
+//   $.each(path2, function(i, el) {
+//     var l = el.getTotalLength();
+//     path[i] = el;
+//     length[i] = l;
+
+//     $(el).css({
+//       "strokeDasharray": l + ' ' + l,
+//       "strokeDashoffset": l
+//     });
+//   });
+
+//   handle = 0;
+// }
+
+
+// var draw = function() {
+
+//   SVG.select("ellipse").each(function() {
+//     this.animate(2000, '>', 0).attr({ stroke: '#f03' });
+//   });
+
+//   SVG.select("rect").each(function() {
+//     this.animate(2000, '>', 0).attr({ stroke: '#f03' });
+//   });
+
+//   var progress = current_frame/total_frames;
+//   if (progress > 1) {
+//    window.cancelAnimationFrame(handle);
+//  } else {
+//    current_frame++;
+//    for(var j=0; j<path.length;j++){
+//      path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+//    }
+//    handle = window.requestAnimationFrame(draw);
+//  }
+// };
+
+// init();
+// draw();
+
+// var rerun = function() {
+//   var old = document.getElementsByTagName('div')[0];
+//   old.parentNode.removeChild(old);
+//   document.getElementsByTagName('body')[0].appendChild(myobj);
+//   init();
+//   draw();
+// };
+
+var vivus = new Vivus('svg-drawing', {
+  type: "oneByOne",
+  duration: 1000
+  // file: "assets/img/process-diagram-simple.svg"
+});
+vivus.play();
 
 
 })();
